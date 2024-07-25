d70a479f094af9815fa86ad057504dab3c903759
Lamda7879 <gilangradiyat03@gmail.com>
2024-07-25 11:12:36 +0700

    Membuat operasi file

--- app/src/main/java/com/lamdapratama/vsga2024/MainActivity.java
+++ app/src/main/java/com/lamdapratama/vsga2024/MainActivity.java
@@ -1,6 +1,9 @@
 package com.lamdapratama.vsga2024;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +11,16 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    public static final String FILENAME = "nama_file.txt";
+    Button buatFile, ubahFile, bacaFile, hapusFile;
+    TextView textBaca;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +32,95 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        buatFile = findViewById(R.id.btn_buat_file);
+        ubahFile = findViewById(R.id.btn_ubah_file);
+        bacaFile = findViewById(R.id.btn_baca_file);
+        hapusFile = findViewById(R.id.btn_hapus_file);
+        textBaca = findViewById(R.id.text_baca);
+
+        buatFile.setOnClickListener(this);
+        ubahFile.setOnClickListener(this);
+        bacaFile.setOnClickListener(this);
+        hapusFile.setOnClickListener(this);
+    }
+
+    void buatFile() {
+        String isiFile = "Coba isi Data File Text";
+        File file = new File(getFilesDir(), FILENAME);
+
+        FileOutputStream outputStream;
+        try{
+            file.createNewFile();
+            outputStream = new FileOutputStream(file);
+            outputStream.write(isiFile.getBytes());
+            outputStream.flush();
+            outputStream.close();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    void ubahFile() {
+        String ubah = "Update Isi Data File Text";
+        File file = new File(getFilesDir(), FILENAME);
+        FileOutputStream outputStream;
+        try {
+            file.createNewFile();
+            outputStream = new FileOutputStream(file, false);
+            outputStream.write(ubah.getBytes());
+            outputStream.flush();
+            outputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    void bacaFile(){
+        File sdCard = getFilesDir();
+        File file = new File(sdCard, FILENAME);
+
+        if(file.exists()){
+            StringBuilder text = new StringBuilder();
+
+            try{
+                BufferedReader br = new BufferedReader(new FileReader(file));
+
+                String line = br.readLine();
+
+                while(line != null){
+                    text.append(line);
+                    line = br.readLine();
+                }
+                br.close();
+            }catch (Exception e){
+                System.out.println("Error: " + e.getMessage());
+            }
+            textBaca.setText(text.toString());
+        }
+    }
+
+    void hapusFile(){
+        File file = new File(getFilesDir(), FILENAME);
+        if (file.exists()) {
+            file.delete();
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        jalankanPerintah(v.getId());
+    }
+
+    public void jalankanPerintah(int id) {
+        if(id == R.id.btn_buat_file){
+            buatFile();
+        }else if(id == R.id.btn_baca_file){
+            bacaFile();
+        }else if(id == R.id.btn_ubah_file){
+            ubahFile();
+        }else if(id == R.id.btn_hapus_file){
+            hapusFile();
+        }
     }
 }
