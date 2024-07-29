b8674dda67ae6467872f738f5ef631a77f79e034
Lamda7879 <gilangradiyat03@gmail.com>
2024-07-29 09:30:43 +0700

    Membuat logic tambah catatan

--- app/src/main/java/com/lamdapratama/vsga2024/MainActivity.java
+++ app/src/main/java/com/lamdapratama/vsga2024/MainActivity.java
@@ -1,8 +1,12 @@
 package com.lamdapratama.vsga2024;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -20,5 +24,22 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId() == R.id.menu_tambah){
+            Intent i = new Intent(this, TambahActivity.class);
+            startActivity(i);
+            return true;
+        }
+        return false;
     }
 }
