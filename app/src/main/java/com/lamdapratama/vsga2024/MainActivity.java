787217918faf2550aa7067741cd657f776c5ba0f
Lamda7879 <gilangradiyat03@gmail.com>
2024-08-08 07:26:28 +0700

    Mencoba Sensor Lux

--- app/src/main/java/com/lamdapratama/vsga2024/MainActivity.java
+++ app/src/main/java/com/lamdapratama/vsga2024/MainActivity.java
@@ -1,6 +1,17 @@
 package com.lamdapratama.vsga2024;
 
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +19,12 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    private SensorManager sensorManager;
+    private Sensor mLight;
+    private TextView tvSensor;
+    private ImageView ivIndicator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +36,43 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        mLight = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+        tvSensor = findViewById(R.id.tvSensor);
+        ivIndicator = findViewById(R.id.ivIndicator);
     }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        float lux = event.values[0];
+        tvSensor.setText("Lux: " + lux);
+
+        if (lux < 10000) {
+            ivIndicator.setColorFilter(Color.GREEN);
+        } else if(lux >= 10000 && lux < 20000){
+            ivIndicator.setColorFilter(Color.YELLOW);
+        }
+        else {
+            ivIndicator.setColorFilter(Color.RED);
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(this, mLight, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
+    }
+
 }
