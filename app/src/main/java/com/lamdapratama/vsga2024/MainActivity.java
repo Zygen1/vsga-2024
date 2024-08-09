ae81a86c5912a0dc179c44ab46dc104f02d9b4d4
Lamda7879 <gilangradiyat03@gmail.com>
2024-08-09 09:36:53 +0700

    Mencoba Sensor Cahaya

--- app/src/main/java/com/lamdapratama/vsga2024/MainActivity.java
+++ app/src/main/java/com/lamdapratama/vsga2024/MainActivity.java
@@ -1,6 +1,11 @@
 package com.lamdapratama.vsga2024;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +13,11 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    private SensorManager manager;
+    private Sensor mLight;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +29,33 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        manager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        mLight = manager.getDefaultSensor(Sensor.TYPE_LIGHT);
+
+        textView = findViewById(R.id.textView);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        float lux = event.values[0];
+
+        // TODO: tampilkan ke user
+        textView.setText(getString(R.string.nilai_sensor_cahaya_n, lux));
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) { }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        manager.registerListener(this, mLight, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        manager.unregisterListener(this);
     }
 }
